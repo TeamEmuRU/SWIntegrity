3c95c532c975e08680d5e895423a0ce1753bf8db
creamchzdragon <jtwalder@gmail.com>
2018-02-23 22:48:04 -0500

    added -all option back in

--- src/SWint.java
+++ src/SWint.java
@@ -37,7 +37,14 @@ public class SWint {
 			//TODO get all c++ files
 			r.openCpp();
 		}
-	
+		else if(args[0].equals("-all")) {
+
+			r.openCpp();
+			r.openAda();
+			r.openJava();
+
+
+		}
 		//then is should be a file name 
 		else {
 			
