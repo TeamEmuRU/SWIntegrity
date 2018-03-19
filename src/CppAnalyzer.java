fbb494ca60853d6fdce2d27c3ef0c4cc7cdd02e6
Abby Beizer <abbybeizer@gmail.com>
2018-03-18 20:44:39 -0400

    started logic for parsing variables, untested

--- src/CppAnalyzer.java
+++ src/CppAnalyzer.java
@@ -35,7 +35,6 @@ public class CppAnalyzer extends Analyzer
 		 * TODO: Process the line for variable names
 		 */
 		
-		
 		try {
 			BufferedReader br = new BufferedReader(new FileReader(filename));
 			String line;
@@ -65,10 +64,11 @@ public class CppAnalyzer extends Analyzer
 					}
 				}
 				
-				//removes single-line comments in the format "//"
-				line = line.replaceAll("//.*$", "");
 				//removes single-line comments in the format "/* --- */"
 				line = line.replaceAll("/\\*.*\\*/", "");
+				//removes single-line comments in the format "//"
+				line = line.replaceAll("//.*$", "");
+			
 				//removes all characters following "/*" and flags the next line as a continuation of a multi-line comment
 				if(line.contains("/*"))
 				{
@@ -82,12 +82,60 @@ public class CppAnalyzer extends Analyzer
 				if(line.length() == 0) {
 					continue;
 				}
-				else
-				{
-					//variables
-					//conditional statements
-				}
 				
+				
+				/* Then locate conditional statements */
+					//TODO: Conditional statements
+				
+				
+				/* Then locate variables */
+				
+				//Split the line into an array of individual words
+				String[] split = line.split(" ");
+				for(int i = 0; i < split.length - 1; i++)
+				{
+					//Look for patterns in each word
+					//The labeled switch cases catch primitive types
+					switch(split[i])
+					{
+					//TODO: Do something with the variables found
+					//type name = value;
+					case "bool"		: { variablesList.add(split[i + 1]); break; }
+					case "char"		: { variablesList.add(split[i + 1]); break; }
+					case "int"		: { variablesList.add(split[i + 1]); break; }
+					case "float"	: { variablesList.add(split[i + 1]); break; }
+					case "double"	: { variablesList.add(split[i + 1]); break; }
+					case "wchar_t"	: { variablesList.add(split[i + 1]); break; }
+					//default case catches any non-primitive types
+					default : {
+						try {
+							//Formats for declaration include
+							//Var name;			`	--> name = Var(); 	or	name = new Var();
+							//Var name = Var();		-->				name = new Var();
+							
+							//To resolve this issue, check to see if there is only one remaining word before a semicolon is encountered
+							//If this is true, then the next word is the name of a variable
+							//catches cases Var foo; and Var foo ;
+							//TODO: Properly handle this ArrayIndexOutOfBoundsException check before attempting to evaluate i + 2
+							if(split[i + 1].contains(";") || split[i + 2].equals(";"))
+							{
+								variablesList.add(split[i + 1]);
+							}
+							//If the next word is followed by an assignment, then the next word is the name of a variable
+							//catches cases Var foo=bar; 	and   Var foo= bar; 	and   Var foo = bar;
+							//TODO: Properly handle this ArrayIndexOutOfBoundsException check before attempting to evaluate i + 2
+							else if(split[i + 1].contains("=") || split[i + 2].equals("="))
+							{
+								variablesList.add(split[i + 1]);
+							}
+						}
+						catch(ArrayIndexOutOfBoundsException ai)
+						{
+							continue;
+						}
+					}//end default
+					}//end switch statement
+				}//end for loop	
 			
 			}
 			
@@ -106,6 +154,7 @@ public class CppAnalyzer extends Analyzer
 		
 	}
 
+	
 	@Override
 	protected void analyze(String filename) {
 		// TODO Auto-generated method stub
