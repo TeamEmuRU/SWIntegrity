39555aa13bd2c4faf318737c2f404d4bd375c154
Abby Beizer <abbybeizer@gmail.com>
2018-03-21 12:25:30 -0400

    Added print statements to demonstrate parser

--- src/CppAnalyzer.java
+++ src/CppAnalyzer.java
@@ -109,7 +109,7 @@ public class CppAnalyzer extends Analyzer
 			//Remove any blank words in the ArrayList
 			if(!word.equals(""))
 			{
-				result+=word+" ";
+				result += word + " ";
 			}
 		}
 		
@@ -139,6 +139,8 @@ public class CppAnalyzer extends Analyzer
 	 */
 	public void extractVariables(String s) 
 	{
+		s = flattenCode(s);
+		
 		//Create an array of all words separated by a space
 		String words[] = s.split(" ");
 		//Transform the array into an ArrayList
@@ -187,6 +189,7 @@ public class CppAnalyzer extends Analyzer
 	@Override
 	public void parse(String filename) {
 		
+		
 		/*
 		 * First, remove any comments from the line
 		 * 		TODO: ignore comment symbols that are used as string literals
@@ -234,7 +237,10 @@ public class CppAnalyzer extends Analyzer
 					mComment = true;
 				}
 				
+				System.out.println(line);
+				
 				//After removal of comments, process remaining characters
+				
 				
 				//ignore lines that are blank
 				if(line.length() == 0) {
@@ -242,64 +248,17 @@ public class CppAnalyzer extends Analyzer
 				}
 				
 				
+				
 				/* Then locate conditional statements */
-					//TODO: Conditional statements
+				//TODO: Conditional statements
 				
 				
 				/* Then locate variables */
-				
-				//Split the line into an array of individual words
-				String[] split = line.split(" ");
-				for(int i = 0; i < split.length - 1; i++)
-				{
-					//Look for patterns in each word
-					//The labeled switch cases catch primitive types
-					switch(split[i])
-					{
-					//TODO: Do something with the variables found
-					//type name = value;
-					case "bool"		: { variablesList.add(split[i + 1]); break; }
-					case "char"		: { variablesList.add(split[i + 1]); break; }
-					case "int"		: { variablesList.add(split[i + 1]); break; }
-					case "float"	: { variablesList.add(split[i + 1]); break; }
-					case "double"	: { variablesList.add(split[i + 1]); break; }
-					case "wchar_t"	: { variablesList.add(split[i + 1]); break; }
-					//default case catches any non-primitive types
-					default : {
-						try {
-							//Formats for declaration include
-							//Var name;			`	--> name = Var(); 	or	name = new Var();
-							//Var name = Var();		-->				name = new Var();
-							
-							//To resolve this issue, check to see if there is only one remaining word before a semicolon is encountered
-							//If this is true, then the next word is the name of a variable
-							//catches cases Var foo; and Var foo ;
-							//TODO: Properly handle this ArrayIndexOutOfBoundsException check before attempting to evaluate i + 2
-							if(split[i + 1].contains(";") || split[i + 2].equals(";"))
-							{
-								variablesList.add(split[i + 1]);
-							}
-							//If the next word is followed by an assignment, then the next word is the name of a variable
-							//catches cases Var foo=bar; 	and   Var foo= bar; 	and   Var foo = bar;
-							//TODO: Properly handle this ArrayIndexOutOfBoundsException check before attempting to evaluate i + 2
-							else if(split[i + 1].contains("=") || split[i + 2].equals("="))
-							{
-								variablesList.add(split[i + 1]);
-							}
-						}
-						catch(ArrayIndexOutOfBoundsException ai)
-						{
-							continue;
-						}
-					}//end default
-					}//end switch statement
-				}//end for loop	
+				//TODO: Prevent opening file twice
+				String file = openFile(filename);
+				extractVariables(file);
 			
 			}
-			
-		
-			
-
 		}
 		catch(FileNotFoundException fnf)	//from FileReader
 		{
@@ -315,7 +274,6 @@ public class CppAnalyzer extends Analyzer
 	
 	@Override
 	protected void analyze(String filename) {
-		// TODO Auto-generated method stub
 		
 	}
 
