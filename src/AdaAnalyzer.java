0c0323d1ce7a20cc308968d2727c1bbb783e02eb
Tyler W. <jtwalder@gmail.com>
2018-03-25 20:00:38 -0400

    "added basic variable tracking for AdaAnalyzer"

--- src/AdaAnalyzer.java
+++ src/AdaAnalyzer.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.HashSet;
@@ -5,6 +6,7 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import java.util.Stack;
 
 public class AdaAnalyzer extends Analyzer{
 	List<Variable> variables;
@@ -22,7 +24,7 @@ public class AdaAnalyzer extends Analyzer{
 		this.variables = new LinkedList<>();
 		this.literals = new LinkedList<>();
 		this.symbolToLine = new HashMap<>();
-		String[] specialSymbols= {":",";",")","(","+","-","/","\\","*","\\\"","="};
+		String[] specialSymbols= {":",";",")","(","+","-","/","\\","*","\\\"","\n","\t","\r","=","."};
 		String[] keyWords= {"abort","else","new","return","abs","elsif","not","reverse",
 				"abstract","end","null","accept","entry","select","access","exception","of",
 				"separate","aliased","exit","or","subtype","all","others","synchronized","and",
@@ -41,13 +43,72 @@ public class AdaAnalyzer extends Analyzer{
 	public void parse(String filename) {
 		// TODO Auto-generated method stub
 		String fileContents=openFile(filename);
-		
+		fileContents=flattenCodeAndMap(fileContents);
+		extractVariables(fileContents);
 	}
-	private String flattenCodeAndMap(String file) {
-		for(String symbol:specialSymbols) {
-			file.replace(symbol, " "+symbol+" ");
+	public boolean isVarName(String s) {
+		return !keyWords.contains(s)&&!specialSymbols.contains(s);
+	}
+	public void extractVariables(String file) {
+		String[] words=file.split(" ");
+		int scopeID=0;
+		Stack<String> scopes=new Stack<>();
+		for(int i=0;i<words.length;i++) {
+			if(i+2<words.length&&isVarName(words[i])&&words[i+1].equals(":")&&isVarName(words[i+2])) {
+				variables.add(new Variable(words[i],words[i+2],scopes.toString(),symbolToLine.get(i)));
+			}
+			if(words[i].equals("end")||words[i].equals("else")||words[i].equals("elsif")) {
+				scopes.pop();
+			}
+			if((words[i].equals("if")||words[i].equals("else")||words[i].equals("elif")||words[i].equals("loop")||words[i].equals("case"))&&!words[i+1].equals(";")) {
+				scopes.push(words[i]+scopeID);
+				scopeID++;
+			}
+			if(words[i].equals("function")||words[i].equals("procedure")||words[i].equals("body")){
+				scopes.push(words[i+1]+scopeID);
+				scopeID++;
+			}
 		}
-		return null;
+		System.out.println(variables);
+	}
+	public String flattenCodeAndMap(String file) {
+		for(String symbol:specialSymbols) {
+			file=file.replace(symbol, " "+symbol+" ");
+		}
+		file=file.replaceAll("\t", "");
+		String result="";
+		int symbolID=0;
+		int lineID=1;
+		boolean inComment=false;
+		String words[]=file.split(" ");
+		ArrayList<String> words2=new ArrayList<>(words.length);
+		//remove spaces
+		for(String word :words) {
+			if(!word.equals(""))
+				words2.add(word);
+		}
+		
+		for(int i=0;i<words2.size();i++) {
+			if(words2.get(i).equals("-")&&words2.get(i+1).equals("-")) {
+				inComment=true;
+			}
+			if(words2.get(i).equals("\n")||words2.get(i).equals("\r")) {
+				lineID++;
+				if(inComment)
+					inComment=false;
+			}
+			else if(inComment);
+			else if(words2.get(i).equals(""));
+			else {
+				result+=words2.get(i)+" ";
+				symbolToLine.put(symbolID, lineID);
+				symbolID++;
+			}
+			//System.out.println(words[i]);
+		}
+		
+		
+		return result;
 	}
 
 	@Override
