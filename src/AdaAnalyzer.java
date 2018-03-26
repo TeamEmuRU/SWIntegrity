e68f62efa72e4d7a6612ff400a996ea837864c34
Tyler W. <jtwalder@gmail.com>
2018-03-26 12:21:11 -0400

    "added comments to adaAnalyzer"

--- src/AdaAnalyzer.java
+++ src/AdaAnalyzer.java
@@ -11,6 +11,7 @@ import java.util.Stack;
 public class AdaAnalyzer extends Analyzer{
 	List<Variable> variables;
 	List<String> literals;
+	//mapping from symbol to line lookup
 	Map<Integer,Integer> symbolToLine;
 	//symbols that are recognized as part of words
 	Set<String> specialSymbols;
@@ -18,12 +19,16 @@ public class AdaAnalyzer extends Analyzer{
 	
 	
 	
-
+	/**
+	 * constructor for ada analyzer class, it creates the lists necessary for collection of data
+	 */
 	public AdaAnalyzer() {
 		super();
+		
 		this.variables = new LinkedList<>();
 		this.literals = new LinkedList<>();
 		this.symbolToLine = new HashMap<>();
+		//initialize lists of character and keywords
 		String[] specialSymbols= {":",";",")","(","+","-","/","\\","*","\\\"","\n","\t","\r","=",".","\""};
 		String[] keyWords= {"abort","else","new","return","abs","elsif","not","reverse",
 				"abstract","end","null","accept","entry","select","access","exception","of",
@@ -33,6 +38,7 @@ public class AdaAnalyzer extends Analyzer{
 				"case","in","protected","until","constant","interface","use","is","raise","declare",
 				"range","when","delay","limited","record","while","delta","loop","rem","with","digits",
 				"renames","do","mod","requeue","xor"};
+		//init sets and add the lists to the set for quicker lookup
 		this.specialSymbols=new HashSet<>();
 		this.keyWords=new HashSet<>();
 		this.specialSymbols.addAll(Arrays.asList(specialSymbols));
@@ -40,106 +46,167 @@ public class AdaAnalyzer extends Analyzer{
 		
 	}
 	@Override
+	/**
+	 * parses file for important information like variables and literals
+	 * 
+	 */
 	public void parse(String filename) {
 		// TODO Auto-generated method stub
 		String fileContents=openFile(filename);
 		fileContents=flattenCodeAndMap(fileContents);
 		extractVariables(fileContents);
 	}
+	/**
+	 * checks if words is keyword or symbol 
+	 * @param s word to check
+	 * @return if word is a proper variable name
+	 */
 	public boolean isVarName(String s) {
 		return !keyWords.contains(s)&&!specialSymbols.contains(s);
 	}
+	/**
+	 * pulls variables and literals from file
+	 * @param file file to be analyzed, must be flattened to work
+	 */
 	public void extractVariables(String file) {
+		//split the file by spaces for easy manipulation
 		String[] words=file.split(" ");
+		//set a scope id to define scopes
 		int scopeID=0;
+		//set a stack to track scopes
 		Stack<String> scopes=new Stack<>();
-		boolean inComment=false;
-		String comment="";
+		//set a varaible to track if the extracter is in a literal
+		boolean inLiteral=false;
+		//set variable to build literal
+		String literal="";
+		//begin looking through each word
 		for(int i=0;i<words.length;i++) {
+			//check to see if this character is the begining or the end of a literal
 			if(words[i].equals("\"")) {
-				inComment=!inComment;
-				comment=comment.replace("\"", "");
-				if(!comment.equals(""))
-					literals.add(comment);
+				//switch the literal tracker on/off
+				inLiteral=!inLiteral;
+				//remove all extra qoutes in literal
+				literal=literal.replace("\"", "");
+				//ignore literal if it is empty
+				if(!literal.equals("")) {
+					//add literal to list
+					literals.add(literal);
+					literal="";
+				}
 			}
-			if(!inComment) {
+			//analyze words if not in literal
+			if(!inLiteral) {
+				//check to see if literal is a variable initialization
 			if(i+2<words.length&&isVarName(words[i])&&words[i+1].equals(":")&&isVarName(words[i+2])) {
 				variables.add(new Variable(words[i],words[i+2],scopes.toString(),symbolToLine.get(i)));
 			}
+			//check for the end of a scope and pop if so
 			if(words[i].equals("end")||words[i].equals("else")||words[i].equals("elsif")) {
 				scopes.pop();
 			}
+			//check to see if the word is the begining of an anonimous scope and add scope to stack
 			if((words[i].equals("if")||words[i].equals("else")||words[i].equals("elif")||words[i].equals("loop")||words[i].equals("case"))&&!words[i+1].equals(";")) {
-				scopes.push(words[i]+scopeID);
+				scopes.push(words[i]+"-"+scopeID);
 				scopeID++;
 			}
+			//check to see if the word is the begining of a named scope and that scope to the stack
 			if(words[i].equals("function")||words[i].equals("procedure")||words[i].equals("body")){
 				scopes.push(words[i+1]+"-"+scopeID);
 				scopeID++;
 			}
+			//check for assignment
 			if(words[i].equals(":")&&words[i+1].equals("=")) {
+				//set stack to catch scopes f we need to 
 				Stack<String> tempScope=new Stack<>();
+				//flag for found variable
 				boolean found=false;
+				//keep looking until we find the variable
 				while(!found&&!scopes.isEmpty()) {
-				
+				//check each variable
 					for(Variable var:variables) {
+						//check each variable to see if this words is that variable
 						if(var.getName().equals(words[i-1])&&var.getScope().equals(scopes.toString())) {
+							//start loop to collect assignment
 							int index=i+1;
+							//set assigment to capture assignment
 							String assignment="";
+							//loop and collect until end of statement 
 							while(!words[index].equals(";")) {
 								assignment+=words[index]+" ";
 								index++;
 							}
+							//add assignment to list and record it'sline
 							var.getAssignments().put(symbolToLine.get(i), assignment);
+							//signify that we found it
 							found=true;
 						}
 					}
+					//back up a scope if we couldnt find the variable
 					if(!found&&!scopes.isEmpty()) {
 						tempScope.push(scopes.pop());
 					}
 					
 				}
+				//revert the scopes if need be
 				while(!tempScope.isEmpty()) {
 					scopes.push(tempScope.pop());
 				}
 			}
 			}
 			else {
-				comment+=words[i];
+				//other wise since we're in a literal we add the word to the string
+				literal+=words[i];
 			}
 		}
 		System.out.println(variables);
 		System.out.println(literals);
 	}
+	/**
+	 * flattens code and records symbol's lines
+	 * @param file file to flatten
+	 * @return a string with no new lines with a space seperating every symbol
+	 */
 	public String flattenCodeAndMap(String file) {
+		//add spaces in between special symbols and other words
 		for(String symbol:specialSymbols) {
 			file=file.replace(symbol, " "+symbol+" ");
 		}
+		//remove tabs
 		file=file.replaceAll("\t", "");
+		//set string for the result of this method
 		String result="";
+		//set symbol id to keep track of symbols
 		int symbolID=0;
+		//set line id to keep track of lines
 		int lineID=1;
+		//set flag to track comments
 		boolean inComment=false;
+		//turn string into array
 		String words[]=file.split(" ");
+		//create array for non empty strings
 		ArrayList<String> words2=new ArrayList<>(words.length);
 		//remove spaces
 		for(String word :words) {
 			if(!word.equals(""))
 				words2.add(word);
 		}
-		
+		//go through each word
 		for(int i=0;i<words2.size();i++) {
+			//if it is a cooment begin to ignore this line
 			if(words2.get(i).equals("-")&&words2.get(i+1).equals("-")) {
 				inComment=true;
 			}
+			//when the end of the line is reached, turn the comment tracker off and increment line counter
 			if(words2.get(i).equals("\n")) {
 				lineID++;
 				if(inComment)
 					inComment=false;
 			}
+			//ignore \r if it is in a comment or if it is empty
 			else if(words2.get(i).equals("\r"));
 			else if(inComment);
 			else if(words2.get(i).equals(""));
+			//add word to result and track its line
 			else {
 				result+=words2.get(i)+" ";
 				symbolToLine.put(symbolID, lineID);
