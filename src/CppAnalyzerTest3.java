69bd9d8ffce5dbd347a6fff79a7d197e07a3fb05
Tyler W. <jtwalder@gmail.com>
2018-04-02 13:01:24 -0400

    "added example junit test"

---
+++ src/CppAnalyzerTest3.java
@@ -0,0 +1,47 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class CppAnalyzerTest3 {
+	CppAnalyzer cpp=new CppAnalyzer();
+	@Test
+	void testParse() {
+		
+		cpp.parse("TestFiles/hello.java");
+		assertTrue(cpp.getFileContents().equals("import java.util. * ; public class hello { public static void main ( String [ ] args ) { System.out.println ( \" Hello Word \" ) ; } }"));
+		
+	}
+
+	@Test
+	void testAnalyze() {
+		cpp.analyze("TestFiles/hello.java");
+		assertTrue(cpp.getFileContents().equals("import java.util. * ; public class hello { public static void main ( String [ ] args ) { System.out.println ( \" Hello Word \" ) ; } }"));
+		assertTrue(cpp.getVariablesList().size()==4);
+	}
+
+	@Test
+	void testCppAnalyzer() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testExtractLiterals() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testFlattenCode() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testExtractVariables() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testFileContains() {
+		fail("Not yet implemented");
+	}
+
+}
