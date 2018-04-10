e4b751a8a71e85348f925fe724af26dbd0af4a13
Tyler W. <jtwalder@gmail.com>
2018-04-09 21:28:24 -0400

    added cpp test File

---
+++ TestFiles/danglingPointerTest3.cpp
@@ -0,0 +1,17 @@
+// Deallocating a memory pointed by ptr causes
+// dangling pointer
+#include <stdlib.h>
+#include <stdio.h>
+int main()
+{
+    int * ptr = (int *)malloc(sizeof(int));
+ 
+    // After below free call, ptr becomes a 
+    // dangling pointer
+    delete ptr ; 
+     
+    // No more a dangling pointer
+    //ptr = NULL ;
+}
+//# nclude < stdlib . h > # include < stdio . h > int main( ) { int * ptr = (int * ) malloc ( sizeof ( int ) ); int * ptr = ( int * ) malloc ( sizeof ( int ) ) ;
+    
