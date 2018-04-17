3e49a9015ae28603065c757048602a3026655d1b
antakij1 <antakij1@students.rowan.edu>
2018-04-17 18:49:45 -0400

    Added helloAccessTypes.adb, containing access type code

---
+++ TestFiles/helloAccessTypes.adb
@@ -0,0 +1,17 @@
+with Ada.Text_IO, Ada.Unchecked_Deallocation; use Ada.Text_IO,Ada.Unchecked_Deallocation;
+procedure Hello is
+type Vector     is array (Integer range <>) of Float;
+type Vector_Ref is access Vector;
+--hello this is a test comment
+procedure Free is new Ada.Unchecked_Deallocation(Object => Vector, Name => Vector_Ref);
+VA, VB: Vector_Ref;
+V : Vector;
+begin
+   VA := new Vector(1..10);
+   VB := VA;
+   Free(VA);
+   V:=VB;
+   Put_Line ("Hello WORLD!");
+   i:Integer;
+   i:=0;
+end Hello;
