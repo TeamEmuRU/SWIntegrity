90237d30525c51acc2decdd5eba78b7606c4bcc0
antakij1 <antakij1@students.rowan.edu>
2018-04-18 09:41:31 -0400

    Uses variable class, vulnerability tracks line numbers

--- src/JavaAnalyzer.java
+++ src/JavaAnalyzer.java
@@ -17,83 +17,112 @@ import java.util.Set;
 import java.util.HashSet;
 import java.util.regex.Pattern;
 import java.util.regex.Matcher;
+import java.util.HashMap;
+import java.util.Map;
 
 public class JavaAnalyzer extends Analyzer {
-	private Set<String> variablesList;
+	private Set<Variable> variablesList;
 	private Set<String> literalsList;
+	private Set<String> typeList;
+	private HashMap<String, ArrayList<Integer>> symbolToLine;
 	private Pattern NON_ALPHA_NUMERIC;
+	private int lineNumber = 1;
 
 
-    /**
-     * The main constructor. Initializes list of variables.
-     */
+	/**
+	 * The main constructor. Initializes list of variables.
+	 */
 	public JavaAnalyzer() {
 		super();
 		//instantiate map of variables
 		variablesList = new HashSet<>();
 		literalsList = new HashSet<>();
+		typeList = new HashSet<>();
+		symbolToLine = new HashMap<>();
+		createTypeList();
 		//Create a regex pattern to catch special characters
 		String regex = "[\\W&&\\S]";
 		NON_ALPHA_NUMERIC = Pattern.compile(regex);
 	}
 
-    /**
-    * Returns the set of variables.
-	* @return the set of variables.
-    */
-	public Set<String> getVariablesList() {
+	private void createTypeList(){
+		typeList.add("int");
+		typeList.add("char");
+		typeList.add("boolean");
+		typeList.add("short");
+		typeList.add("byte");
+		typeList.add("long");
+		typeList.add("float");
+		typeList.add("double");
+	}
+
+	/**
+	 * Returns the set of variables.
+	 *
+	 * @return the set of variables.
+	 */
+	public Set<Variable> getVariablesList() {
 		return variablesList;
 	}
-	
+
 	/**
-    * Returns the set of String literals.
-	* @return the set of String literals .
-    */
+	 * Returns the set of String literals.
+	 *
+	 * @return the set of String literals .
+	 */
 	public Set<String> getLiteralsList() {
 		return literalsList;
 	}
-	
-    /**
-	* Reads an array of characters, extracts String literals represented inside, and stores them.
-	* @param arr an array of characters
-    */
-	private void extractLiterals(char[] arr) 
-	{
-	    boolean inString = false;
-	    String literal = "";
-	    for(int i=0; i<arr.length; i++) 
-	    {
-	        if(arr[i] == '\"') 
-	        {
-			    inString = !inString;
-			    if(literal.length() != 0) 
-			    {
+
+	/**
+	 * Reads an array of characters, extracts String literals represented inside, and stores them.
+	 *
+	 * @param arr an array of characters
+	 */
+	private void extractLiterals(char[] arr) {
+		boolean inString = false;
+		String literal = "";
+		for (int i = 0; i < arr.length; i++) {
+			if (arr[i] == '\"') {
+				inString = !inString;
+				if (literal.length() != 0) {
 					literalsList.add(literal);
 					literal = "";
-			    }
-	        }
-	        else if(inString) 
-	        {
-	        	literal += arr[i];
-	        }
-	    }
+				}
+			} else if (inString) {
+				literal += arr[i];
+			}
+		}
 	}
 
-    /**
-     * Pads non-alphanumeric characters with spaces to make the code easier to work with
-     * @param s the line of code to format
-     * @return the formatted line of code
-     */
-	public String flattenCode(String s)
-	{
+	/**
+	 * Pads non-alphanumeric characters with spaces to make the code easier to work with
+	 *
+	 * @param s the line of code to format
+	 * @return the formatted line of code
+	 */
+	public String flattenCode(String s) {
 		//Remove all whitespace characters that are not regular spaces
-		String finalString = s.trim().replaceAll("[\n|\t|\r|\f]", "");
+		String finalString = s.trim().replaceAll("[\\n|\\t|\\r|\\f]", "");
 		//Separate all special characters
 		Matcher match = NON_ALPHA_NUMERIC.matcher(finalString);
+
+		Pattern alpha = Pattern.compile("[\\w]+");
+		Matcher mm = alpha.matcher(finalString);
+		while(mm.find()){
+			if(symbolToLine.get(mm.group()) == null){
+				ArrayList<Integer> temp = new ArrayList<>();
+				temp.add(lineNumber);
+				symbolToLine.put(mm.group(), temp);
+			}
+			else{
+				symbolToLine.get(mm.group()).add(lineNumber);
+			}
+		}
 		String found = "";
-		while(match.find()) {
+		while (match.find()) {
 			found = match.group();
-			finalString = finalString.replace(found, " "+found+" ");
+			finalString = finalString.replace(found, " " + found + " ");
 		}
 		//Create an array of all words separated by a space
 		String words[] = finalString.split(" ");
@@ -101,41 +130,24 @@ public class JavaAnalyzer extends Analyzer {
 		ArrayList<String> list = new ArrayList<>(Arrays.asList(words));
 		Iterator<String> itty = list.iterator();
 		String result = "";
-		while(itty.hasNext()) {
+		while (itty.hasNext()) {
 			String word = itty.next();
 			//Remove any blank words in the ArrayList
-			if(!word.equals(""))
-			{
-				result+=word+" ";
+			if (!word.equals("")) {
+				result += word + " ";
 			}
 		}
-		return result.trim();
+		return result;
 	}
 
-    /**
-     * Adds all variable names of a given type to the list of variables
-     * @param arr An array of string tokens that make up a line of code
-     * @param type The type of variable to pick out
-     */
-	private void searchForType(String[] arr, String type) {
-        for (int i = 0; i < arr.length - 2; i++) {
-            if (arr[i].equals(type) && !arr[i+2].equals("(")) {
-                if(arr[i+1].equals("[") && !arr[i+3].equals(".")) {
-                    variablesList.add(arr[i+3]);
-                }
-                else {
-                    variablesList.add(arr[i + 1]);
-                }
-            }
-        }
-    }
 
-    /**
-     * Extracts variable names from a line of code
-     * @param s The line of code to extract variables from
-     */
-    public void extractVariables(String s)
-    {
+	/**
+	 * Extracts variable names from a line of code
+	 *
+	 * @param s The line of code to extract variables from
+	 */
+	public void extractVariables(String s) {
+		s = s.replace(" \\ t", "");
 		//Create an array of all words separated by a space
 		String words[] = s.split(" ");
 		//Transform the array into an ArrayList
@@ -145,58 +157,103 @@ public class JavaAnalyzer extends Analyzer {
 		//set a switch to keep track of when we are in a string.
 		boolean stringSwitch = false;
 		//the following loop removes Strings. In order to detect variables strings must not be present
-		while(itty.hasNext())
-		{
+		while (itty.hasNext()) {
 			String word = itty.next();
-
 			//if we run into the beginning of a string we turn on the switch and remove the quote character, this happens again at the end of the String
-			if(word.equals("\""))
-			{
+			if (word.equals("\"")) {
 				stringSwitch = !stringSwitch;
 				itty.remove();
 			}
 			//while inside of a string we remove it
-			else if(stringSwitch && !word.equals("\""))
-			{
+			else if (stringSwitch && !word.equals("\"")) {
 				itty.remove();
 			}
 
 		}
 		//put words back into an array
 		words = list.toArray(new String[list.size()]);
-        //extract String and primitive variables
-        searchForType(words, "String");
-        searchForType(words, "boolean");
-        searchForType(words, "byte");
-        searchForType(words, "char");
-        searchForType(words, "short");
-        searchForType(words, "int");
-        searchForType(words, "long");
-        searchForType(words, "float");
-        searchForType(words, "double");
-        //extract all other variables
-        for (int i = 2; i < words.length; i++) {
-            if((!words[i-1].equals(")"))&&(words[i].equals("=") || words[i].equals(";")) && Character.isUpperCase(words[i-2].charAt(0))){
-                if(words[i-1].equals(">")) {
-					variablesList.add(words[i-2]);
-				}
-				else {
-					variablesList.add(words[i - 1]);
-				}
-            }
-        }
-    }
 
+		int scopeID = 0;	//a scope id to differentiate scopes
+		String className = "[A-Z_$][\\w$]*";
+		String variableName = "[\\w$]+";
+		int scope = 0;
+		for(int i = 0; (i <= words.length - 4); i++) {
+			String name = "";
+			String type = "";
+			String assignment = "";
+			if(words[i].equals("{")){
+				scope++;
+			}
+			else if(words[i].equals("}")){
+				scope--;
+			}
+			else{
+			if((typeList.contains(words[i]) || words[i].matches(className)) && words[i+1].matches(variableName) &&  (words[i+2].equals(";") || words[i+2].equals("="))){
+				name = words[i+1];
+				type = words[i];
+				if(words[i+2].equals("=")){
+					int place = i+3;
+					while(!(words[place].equals(";")) && !(words[place] == null)) {
+						assignment += words[place] + " ";
+						place++;
+					}
+				}
+			}
+			else if((typeList.contains(words[i]) || words[i].matches(className)) && words[i+1].matches(variableName) && words[i+2].equals("[") && words[i+3].equals("]") && (words[i+4].equals("=") || words[i+4].equals(";"))){
+				name = words[i+1];
+				type = words[i] + "[]";
+				if(words[i+4].equals("=")){
+					int place = i+5;
+					while(!(words[place].equals(";")) && !(words[place] == null)){
+						assignment += words[place] + " ";
+						place++;
+					}
+				}
+			}
+			else if((typeList.contains(words[i]) || words[i].matches(className)) && words[i+1].equals("[") && words[i+2].equals("]") && words[i+3].equals(variableName) && (words[i+4].equals("=") || words[i+4].equals(";"))){
+				name = words[i+3];
+				type = words[i] + "[]";
+				if(words[i+4].equals("=")){
+					int place = i+5;
+					while(!(words[place].equals(";")) && !(words[place] == null)){
+						assignment += words[place] + " ";
+						place++;
+					}
+				}
+			}
+			else if((typeList.contains(words[i]) || words[i].matches(className)) && words[i+1].equals("<")){
+				int place = i+2;
+				String rest = "";
+				while(!(words[place].equals(">"))){
+					rest += words[place];
+					place++;
+				}
+				name = words[++place];
+				type = words[i] + "<" + rest + ">";
+				if(words[++place].equals("=")){
+					place++;
+					while(!(words[place].equals(";"))){
+						assignment += words[place] + " ";
+					}
+				}
+			}
+			if(!(name.equals(""))){
+				variablesList.add(new Variable(name, type, Integer.toString(scope), assignment + ";", 2));
+			}
+	}
+	}
+	}
 	@Override
 	/*
 	 * Removes comments and extracts variables from source code.
 	 * @param filename the file to be parsed
 	 */
 	public void parse(String filename) {
+		StringBuilder fileBuilder = new StringBuilder();
+		String line = "";
 		try {
 			BufferedReader br = new BufferedReader(new FileReader(filename));
-			String line;
-			String[] cleanLine=null;
+			String[] cleanLine = null;
 			boolean mComment = false;    //whether the current line is a continuation of a multi-line comment
 
 			//Runs until end of file
@@ -238,95 +295,203 @@ public class JavaAnalyzer extends Analyzer {
 				}
 
 				/* Second, extract the literals and variables: */
-				
+
 				extractLiterals(line.toCharArray());
 				line = flattenCode(line);
-				extractVariables(line);
-				//temporary fix
-				Iterator<String> it = variablesList.iterator();
-				Matcher m;
-				while(it.hasNext()) {
-					String element = it.next();
-					m = NON_ALPHA_NUMERIC.matcher(element);
-					if(m.matches()) {
-						it.remove();
-					}
-				}
-
+				fileBuilder.append(line);
+				lineNumber++;
 			}
 			br.close();
-            }
-			catch(FileNotFoundException fnf){	//from FileReader
-				SIT.notifyUser("Error: File " + filename + " could not be parsed.");
+		} catch (FileNotFoundException fnf) {    //from FileReader
+			SIT.notifyUser("Error: File " + filename + " could not be parsed.");
+		} catch (IOException io) {    //from BufferedReader
+			SIT.notifyUser("Error reading the contents of " + filename + ".");
+		}
+		extractVariables(fileBuilder.toString());
+		//temporary fix
+		Iterator<Variable> it = variablesList.iterator();
+		Matcher m;
+		while (it.hasNext()) {
+			Variable element = it.next();
+			m = NON_ALPHA_NUMERIC.matcher(element.getName());
+			if (m.find()) {
+				it.remove();
 			}
-			catch(IOException io){	//from BufferedReader
-				SIT.notifyUser("Error reading the contents of " + filename + "." );
-			}
-			//System.out.println("Variables:");
-			//System.out.println(variablesList);
-			//System.out.println("Literals:");
-	    	//System.out.println(literalsList);
-		
+		}
+		//System.out.println("Variables:");
+		System.out.println(variablesList);
+		//System.out.println("Literals:");
+		//System.out.println(literalsList);
 	}
 
 	@Override
 	protected void analyze(String filename) {
 		parse(filename);
+		System.out.println(symbolToLine);
 	}
-	
-	//This is the java SQL injection vulnerability; hard-coded into the program until the database is implemented
-    /**
-     * Method that analyzes a file for possible vulnerability to SQL injections 
-     * @param fileName the name of the file to be analyzed
-     */
-	public void sqlVuln(String fileName)
-	{
-		String DBkeywords[] = {"SELECT", "UNION", "WHERE", "FROM", "HAVING", "JOIN", "ORDER BY"}; //a list of key words used in SQL
-		String keyInMethods[] = {".NEXT",".READ", ".GET"}; //a list of methods used to obtain input from the user, list can be extended later
-		String contents = "";
-		String currentLine = "";
-		boolean badSQL = false; // a boolean to see if this vulnerability exists
-		
-		try{
-			BufferedReader br = new BufferedReader(new FileReader(fileName));
-		
-			while((currentLine = br.readLine()) != null){
-				contents += currentLine;
-			}
-			//checks the two java library api imports see if being used
-			//if no then skips all analysis to return false no vulnerabilities
-			if(contents.contains("java.sql") || contents.contains("jdbc")){
-				contents = contents.toUpperCase();
-				
-				for(String word : DBkeywords){//iterates through the key word list to see if they
-											  //appear in the string of the program code.
-					
-					//if said keyword appears, checks for specific
-					// statements that hackers use for SQL Injection.
-					// %00 is a null byte used by attackers in many different
-					// types of vulnerabilities.
-					if(contents.contains(word)){
-						
-						//if keywords were found, check to see if the program collects user input
-						for(String inputWord : keyInMethods){
-							
-							//If it does collect user input, check to see if it uses prepared statements
-							//prepared statements are safe. If no prepared statement, not safe.
-							if(contents.contains(inputWord)&& !contents.contains("PREPAREDSTATEMENT")){
-								badSQL = true;
-							}
-							
-						}
-					} 													     
+
+	public class Variable {
+		String name;
+		String type;
+		String scope;
+		//Line on which this variable is created
+		int lineNumber;
+		//line on which the assignment is followed by the reference
+		String assignments;
+
+		public Variable(String name, String type, String scope, String assignments, int line) {
+			super();
+			this.name = name;
+			this.type = type;
+			this.scope = scope;
+			this.lineNumber = line;
+			this.assignments = assignments;
+		}
+
+		public String getName() {
+			return name;
+		}
+
+		public void setName(String name) {
+			this.name = name;
+		}
+
+		public String getType() {
+			return type;
+		}
+
+		public void setType(String type) {
+			this.type = type;
+		}
+
+		public String getScope() {
+			return scope;
+		}
+
+		public void setScope(String scope) {
+			this.scope = scope;
+		}
+
+		public int getLine() {
+			return lineNumber;
+		}
+
+		public void setLine(int line) {
+			this.lineNumber = line;
+		}
+
+		public String getAssignments() {
+			return assignments;
+		}
+
+		public void setAssignments(String assignments) {
+			this.assignments = assignments;
+		}
+
+		@Override
+		public String toString() {
+			return "Variable [name=" + name + ", type=" + type + ", scope=" + scope + ", symbolNumber=" + lineNumber
+					+ ", assignments=" + assignments + "]";
+		}
+
+		@Override
+		public int hashCode() {
+			final int prime = 31;
+			int result = 1;
+			result = prime * result + getOuterType().hashCode();
+			result = prime * result + ((name == null) ? 0 : name.hashCode());
+			result = prime * result + ((scope == null) ? 0 : scope.hashCode());
+			return result;
+		}
+
+		@Override
+		public boolean equals(Object obj) {
+			if (this == obj)
+				return true;
+			if (obj == null)
+				return false;
+			if (getClass() != obj.getClass())
+				return false;
+			Variable other = (Variable) obj;
+			if (!getOuterType().equals(other.getOuterType()))
+				return false;
+			if (name == null) {
+				if (other.name != null)
+					return false;
+			} else if (!name.equals(other.name))
+				return false;
+			if (scope == null) {
+				if (other.scope != null)
+					return false;
+			} else if (!scope.equals(other.scope))
+				return false;
+			return true;
+		}
+
+		private JavaAnalyzer getOuterType() {
+			return JavaAnalyzer.this;
+		}
+
+		//This is the java SQL injection vulnerability; hard-coded into the program until the database is implemented
+
+		/**
+		 * Method that analyzes a file for possible vulnerability to SQL injections
+		 *
+		 * @param fileName the name of the file to be analyzed
+		 */
+		public void sqlVuln(String fileName) {
+			String DBkeywords[] = {"SELECT", "UNION", "WHERE", "FROM", "HAVING", "JOIN", "ORDER BY"}; //a list of key words used in SQL
+			String keyInMethods[] = {".NEXT", ".READ", ".GET"}; //a list of methods used to obtain input from the user, list can be extended later
+			String contents = "";
+			String currentLine = "";
+			ArrayList<Integer> locations = new ArrayList<>();
+			boolean badSQL = false; // a boolean to see if this vulnerability exists
+
+			try {
+				BufferedReader br = new BufferedReader(new FileReader(fileName));
+
+				while ((currentLine = br.readLine()) != null) {
+					contents += currentLine;
 				}
-			} 
+				//checks the two java library api imports see if being used
+				//if no then skips all analysis to return false no vulnerabilities
+				if (contents.contains("java.sql") || contents.contains("jdbc")) {
+					contents = contents.toUpperCase();
+
+					for (String word : DBkeywords) {//iterates through the key word list to see if they
+						//appear in the string of the program code.
+
+						//if said keyword appears, checks for specific
+						// statements that hackers use for SQL Injection.
+						// %00 is a null byte used by attackers in many different
+						// types of vulnerabilities.
+						if (contents.contains(word)) {
+
+							//if keywords were found, check to see if the program collects user input
+							for (String inputWord : keyInMethods) {
+
+								//If it does collect user input, check to see if it uses prepared statements
+								//prepared statements are safe. If no prepared statement, not safe.
+								if (contents.contains(inputWord) && !contents.contains("PREPAREDSTATEMENT")) {
+									badSQL = true;
+									locations.addAll(symbolToLine.get(inputWord));
+								}
+
+							}
+						}
+					}
+				}
+			} catch (IOException e) {
+				System.out.println("FileNotFoundException in "
+						+ "Java SQL analyze");
+			}
+
+			//Display whether possible sql injections were detected
+			System.out.println("At risk of SQL injection: " + badSQL);
+			if(badSQL){
+				System.out.print("Risks located on lines ");
+				System.out.println(locations);
+			}
 		}
-		catch (IOException e){
-			System.out.println("FileNotFoundException in "
-					+ "Java SQL analyze");
-		}
-		
-		//Display whether possible sql injections were detected
-		System.out.println("At risk of SQL injection: "+badSQL);
 	}
 }
