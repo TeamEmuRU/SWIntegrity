29359ae0e4235b187c2708e9c66b1a41f409ce50
Joe Mcilvaine <jomac718@yahoo.com>
2018-10-14 18:04:50 -0400

    Bug Fixes

--- TestFiles/OverFlow.java
+++ TestFiles/OverFlow.java
@@ -1,23 +1,21 @@
 public class OverFlow {
 
+    public static void main(String args[]) {
+
     int a = 5000000;
     int b = 1000000;
     int c = 500;
 
 
-    if ((long) (a + b) < Integer.MAX_VALUE) {
+    if((long)(a +b) <Integer.MAX_VALUE)
+
+    {
         c = a + b;
     }
 
 
     char j = 'a' + 'b';
-    a = 10000;
-    b = 10000;
-    short x = 55 + 10000;
-    byte z = 256 + 246;
-
-
-
 
     c = c + b;
+}
 }
